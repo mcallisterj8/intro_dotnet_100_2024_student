591b3b4f44c0e10317c8e7649af5dfae84c9957c
Jacob McAllister <datadeltaanalysis@protonmail.com>
2024-03-27 15:21:47 -0500

    Cleaned up Program.cs
    Signed-off-by: Jacob McAllister <datadeltaanalysis@protonmail.com>

--- GenericsExamples/Program.cs
+++ GenericsExamples/Program.cs
@@ -1,31 +1,63 @@
 ﻿using GenericsExamples.Models;
 
-List<string> words = new List<string> {"apple", "berry", "cherry", "date"};
-List<int> numbers = new List<int> {12, 16, 18, 24, 80};
-iPhone phoneA = new iPhone {
-                    Brand = "Apple", 
-                    Model = "iPhone 11"
-                };
-Android phoneB = new Android {
-            Brand = "Google", 
-            Model = "Samsung Galaxy 23"
-        };
+iPhone iphoneA = new iPhone
+{
+    Brand = "Apple",
+    Model = "iPhone 11"
+};
 
-iPhone phoneC = new iPhone {
-                    Brand = "Apple", 
-                    Model = "iPhone 12"
-                };
+iPhone iphoneB = new iPhone
+{
+    Brand = "Apple",
+    Model = "iPhone 12"
+};
 
-ISmartphone phoneD = new Android {
-            Brand = "Google", 
-            Model = "Samsung Galaxy 20"
-        };
-List<iPhone> smartphoneList = new List<iPhone> {
-                                        phoneA,
-                                        // phoneB,
-                                        phoneC,
-                                        // phoneD
-                                    };
+iPhone iphoneC = new iPhone
+{
+    Brand = "Apple",
+    Model = "iPhone 8"
+};
+
+Android androidA = new Android
+{
+    Brand = "Google",
+    Model = "Samsung Galaxy 23"
+};
+
+
+Android androidB = new Android
+{
+    Brand = "Google",
+    Model = "Samsung Galaxy 20"
+};
+
+Android androidC = new Android
+{
+    Brand = "Google",
+    Model = "Samsung Galaxy 24"
+};
+
+List<string> words = new List<string> { "apple", "berry", "cherry", "date" };
+List<int> numbers = new List<int> { 12, 16, 18, 24, 80 };
+
+List<iPhone> iPhoneList = new List<iPhone> {
+    iphoneA,
+    iphoneB,
+    iphoneC
+};
+
+List<Android> androidList = new List<Android> {
+    androidA,
+    androidB,
+    androidC
+};
+
+List<ISmartphone> smartPhoneList = new List<ISmartphone> {
+    iphoneA,
+    androidA,
+    iphoneB,
+    androidB
+};
 
 Box<string> boxA = new Box<string>("boxA", "Star Item", words);
 Console.WriteLine(boxA.ToString());
@@ -37,5 +69,77 @@ Console.WriteLine(boxB.ToString());
 
 
 Console.WriteLine("\n=======================================");
-SmartBox<iPhone> boxC = new SmartBox<iPhone>("boxC (smartbox)", phoneA, smartphoneList);
+SmartBox<iPhone> boxC = new SmartBox<iPhone>("boxC (smartbox)", iphoneA, iPhoneList);
+Console.WriteLine(boxC.ToString());using GenericsExamples.Models;
+
+iPhone iphoneA = new iPhone
+{
+    Brand = "Apple",
+    Model = "iPhone 11"
+};
+
+iPhone iphoneB = new iPhone
+{
+    Brand = "Apple",
+    Model = "iPhone 12"
+};
+
+iPhone iphoneC = new iPhone
+{
+    Brand = "Apple",
+    Model = "iPhone 8"
+};
+
+Android androidA = new Android
+{
+    Brand = "Google",
+    Model = "Samsung Galaxy 23"
+};
+
+
+Android androidB = new Android
+{
+    Brand = "Google",
+    Model = "Samsung Galaxy 20"
+};
+
+Android androidC = new Android
+{
+    Brand = "Google",
+    Model = "Samsung Galaxy 24"
+};
+
+List<string> words = new List<string> { "apple", "berry", "cherry", "date" };
+List<int> numbers = new List<int> { 12, 16, 18, 24, 80 };
+
+List<iPhone> iPhoneList = new List<iPhone> {
+    iphoneA,
+    iphoneB,
+    iphoneC
+};
+
+List<Android> androidList = new List<Android> {
+    androidA,
+    androidB,
+    androidC
+};
+
+List<ISmartphone> smartPhoneList = new List<ISmartphone> {
+    iphoneA,
+    androidA,
+    iphoneB,
+    androidB
+};
+
+Box<string> boxA = new Box<string>("boxA", "Star Item", words);
+Console.WriteLine(boxA.ToString());
+
+Console.WriteLine("\n=======================================");
+
+Box<int> boxB = new Box<int>("boxB", 8, numbers);
+Console.WriteLine(boxB.ToString());
+
+
+Console.WriteLine("\n=======================================");
+SmartBox<iPhone> boxC = new SmartBox<iPhone>("boxC (smartbox)", iphoneA, iPhoneList);
 Console.WriteLine(boxC.ToString());
