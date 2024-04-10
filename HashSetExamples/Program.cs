255f837787c33fe32152a3f114f48647b30e1d60
Jacob McAllister <mcallisterj@westerntc.edu>
2024-04-10 10:31:09 -0500

    End of class 4-10-2024
    Signed-off-by: Jacob McAllister <mcallisterj@westerntc.edu>

---
+++ HashSetExamples/Program.cs
@@ -0,0 +1,15 @@
+﻿HashSet<int> hashSet = new HashSet<int> { 1, 2, 3, 4, 5 };
+
+List<int> numbers = new List<int> { 2, 3, 4, 6 };
+
+hashSet.SymmetricExceptWith(numbers); // Will now contain 1, 5
+
+int x;
+bool result = hashSet.TryGetValue(5, out x);
+
+Console.WriteLine($"found => {result}; num: {x}");
+
+// foreach(int elem in hashSet) {
+//     Console.WriteLine(elem);
+// }
+
